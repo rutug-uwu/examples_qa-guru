19d06df53b85fab664b8bdd8b9332a4d1bc6a3da
Ruslan Tugushev <rutug.official@gmail.com>
2025-06-13 17:13:02 +0400

    1 ex - done

---
+++ src/test/java/SearchTests.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class SearchTests {
+    @Test
+    void successfulSearchTest() {
+        open("https://duckduckgo.com/");
+        $("#searchbox_input").setValue("selenide").pressEnter();
+        $("[data-testid=result]").shouldHave(text("https://selenide.org"));
+    }
+}
+    
