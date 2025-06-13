ff1f647bc0cf88da7fba70a13f9741109fd5eee2
Ruslan Tugushev <rutug.official@gmail.com>
2025-06-13 19:11:16 +0400

    Update SearchTests.java

--- src/test/java/SearchTests.java
+++ src/test/java/SearchTests.java
@@ -8,7 +8,7 @@ public class SearchTests {
     @Test
     void successfulSearchTest() {
         open("https://duckduckgo.com/");
-        $("#searchbox_input").setValue("ffffff").pressEnter();
+        $("#searchbox_input").setValue("selenide").pressEnter();
         $("[data-testid=result]").shouldHave(text("https://selenide.org"));
     }
 }
